55ba45a8ba16215772f5a318a5850f71b4ddb32d
Tara van Goch <125177427+vangochtj@users.noreply.github.com>
2023-07-21 17:18:48 +0800

    Add scorekeeping, create game function

--- script.js
+++ script.js
@@ -11,20 +11,57 @@ function getComputerChoice() {
     return rdnInt;
 }
 
+let computerScore = 0;
+let playerScore = 0;
+
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === "rock" && computerSelection === "paper") {
+        computerScore++
         return "You lose! Paper beats rock."
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        playerScore++
         return "You win! Rock beats scissors."
     } else if (playerSelection === "paper" && computerSelection === "rock") {
+        playerScore++
         return "You win! Paper beats rock."
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        computerScore++
         return "You lose! Scissors beat paper."
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        computerScore++
         return "You lose! Rock beats scissors."
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        playerScore++
         return "You win! Scissors beat paper."
     } else {
         return "It's a tie!"
     }
 }
+
+function game() {
+    let playerSelection = prompt('Rock, paper, or scissors?').toLowerCase();
+    let computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+    playerSelection = prompt('Rock, paper, or scissors?').toLowerCase();
+    computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+    playerSelection = prompt('Rock, paper, or scissors?').toLowerCase();
+    computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+    playerSelection = prompt('Rock, paper, or scissors?').toLowerCase();
+    computerSelection = getComputerChoice()
+    console.log(playRound(playerSelection, computerSelection));
+    playerSelection = prompt('Rock, paper, or scissors?').toLowerCase();
+    computerSelection = getComputerChoice()
+    console.log(playRound(playerSelection, computerSelection));
+
+    if (computerScore > playerScore) {
+        console.log("You've lost from the computer.");
+    } else if (playerScore > computerScore) {
+        console.log("You've won from the computer!");
+    } else if (playerScore === computerScore) {
+        console.log("You've tied with the computer.")
+    }
+}
+
+game();
